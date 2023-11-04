12f5b83a8202b3f66476a75968327323598a05f7
kTurek95 <k.turek1995@gmail.com>
2023-11-04 12:56:45 +0000

    added create database function to database module

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import declarative_base, relationship
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 
 Base = declarative_base()
 
@@ -18,3 +18,12 @@ class Password(Base):
     password = Column(String)
     credential_id = Column(Integer, ForeignKey('Credentials.id'))
     credential = relationship('Credential', back_populates='passwords')
+
+
+def create_database():
+    engine = create_engine('sqlite:///database.db', echo=False, future=True)
+    Base.metadata.create_all(engine)
+    sql_session = sessionmaker(bind=engine)
+    session = sql_session()
+
+    return session
