7ff7ddfa77a9a6bf5addb6677b9bb7eb6bd2bce7
kTurek95 <k.turek1995@gmail.com>
2023-11-13 15:28:18 +0000

    added center_window module with center_window function

---
+++ center_window.py
@@ -0,0 +1,7 @@
+def center_window(window):
+    window.update_idletasks()
+    width = window.winfo_width()
+    height = window.winfo_height()
+    x = (window.winfo_screenwidth() // 2) - (width // 2)
+    y = (window.winfo_screenheight() // 2) - (height // 2)
+    window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
