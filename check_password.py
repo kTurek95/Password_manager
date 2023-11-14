af2a58bc5edb37257188da9fc88f2495c8df8f3d
kTurek95 <k.turek1995@gmail.com>
2023-11-14 18:18:55 +0000

    added on_submit method to CheckPassword class in check_password module

--- check_password.py
+++ check_password.py
@@ -33,3 +33,37 @@ class CheckPassword:
         if valid_password == password_from_user:
             return True
         return False
+
+    def on_submit(self, event):
+        """
+        Handles the form submission event in the user interface.
+
+        This method is invoked when the user attempts to submit the form with a password.
+        It checks if the password field is not empty and then verifies the password.
+        If the password is correct, the main window of the application is opened.
+        In case of failed login attempts, the user is presented with appropriate
+        warning messages, and after three failed attempts, access is blocked.
+
+        Parameters:
+            event: The event object passed by the Tkinter event system.
+
+        Returns:
+            None. May close the main application window in case of access being blocked.
+        """
+        global failed_attempts
+        user_password = self.user_password.get()
+        if not user_password.strip():
+            messagebox.showinfo('Info', "You didn't enter the password")
+
+        elif self.check_password():
+            self.root.withdraw()
+            open_main_window(self.root)
+
+        else:
+            if len(user_password) > 1:
+                self.user_password.delete(0, tk.END)
+                failed_attempts += 1
+                messagebox.showwarning('Error', 'Wrong password')
+                if failed_attempts >= 3:
+                    messagebox.showwarning('Error', 'Access blocked')
+                    self.root.destroy()
