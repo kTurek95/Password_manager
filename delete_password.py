9f010c97c009b09de87e3e7148c21d02de68899a
kTurek95 <k.turek1995@gmail.com>
2023-11-16 18:18:57 +0000

    added confrigure_tree method to DeletePassword class in delete_password module

--- delete_password.py
+++ delete_password.py
@@ -16,3 +16,15 @@ class DeletePassword(UpdateTreeview):
             columns=('Choose credential',),
             show='headings',
             height=4, )
+
+        self.configure_tree()
+
+    def configure_tree(self):
+        self.tree.column('Choose credential', width=480)
+        self.tree.heading('Choose credential', text='Choose website credential to delete')
+        self.scrollbar.config(command=self.tree.yview)
+        self.tree.pack()
+        self.scrollbar.place(x=490, rely=0.36, anchor='center', height=99)
+
+        add_tree3_button = ttk.Button(self.tab, text='Submit')
+        add_tree3_button.place(relx=0.5, rely=0.9, anchor="center")
