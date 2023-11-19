28a6e2cb2e8316bcad906d1c261ddb1b8abcd37d
kTurek95 <k.turek1995@gmail.com>
2023-11-19 14:22:49 +0000

    added on_click method to UpdatePassword class in update_password module

--- update_password.py
+++ update_password.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from center_window import center_window
 from update_treeview import UpdateTreeview
 
 
@@ -30,3 +31,32 @@ class UpdatePassword(UpdateTreeview):
         self.scrollbar.config(command=self.tree.yview)
         self.tree.pack()
         self.scrollbar.place(x=490, rely=0.36, anchor='center', height=99)
+
+        add_tree2_button = ttk.Button(self.tab, text='Submit')
+        add_tree2_button.place(relx=0.5, rely=0.9, anchor="center")
+        add_tree2_button.bind('<Button-1>', self.on_click)
+
+    def on_click(self, event):
+        selected_item = self.tree.selection()
+        if selected_item:
+            self.toplevel = tk.Toplevel(self.root)
+            self.item = selected_item
+            self.toplevel.geometry('250x150')
+            center_window(self.toplevel)
+            self.toplevel.transient(self.root)
+            self.toplevel.grab_set()
+            self.toplevel.resizable(False, False)
+
+            user_login_label = ttk.Label(self.toplevel, text='Podaj login: ')
+            user_login_label.place(relx=0.5, rely=0.1, anchor='center')
+
+            self.user_login = ttk.Entry(self.toplevel, show='*')
+            self.user_login.place(relx=0.5, rely=0.25, anchor='center')
+
+            new_password_label = ttk.Label(self.toplevel, text='Podaj nowe hasło: ')
+            new_password_label.place(relx=0.5, rely=0.45, anchor='center')
+
+            self.new_password = ttk.Entry(self.toplevel, show='*')
+            self.new_password.place(relx=0.5, rely=0.60, anchor='center')
+
+
