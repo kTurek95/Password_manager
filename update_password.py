bd79b99ea0bd7c4791bea420c908dd65962e7b3c
kTurek95 <k.turek1995@gmail.com>
2023-11-19 19:08:20 +0000

    added get_data method and on_submit method to UpdatePassword class in update_password module

--- update_password.py
+++ update_password.py
@@ -1,7 +1,10 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, messagebox
+from cipher_tools import encrypt_password
+from database import create_database, Credential
 from center_window import center_window
 from update_treeview import UpdateTreeview
+from password_package import api, main
 
 
 class UpdatePassword(UpdateTreeview):
@@ -47,16 +50,51 @@ class UpdatePassword(UpdateTreeview):
             self.toplevel.grab_set()
             self.toplevel.resizable(False, False)
 
-            user_login_label = ttk.Label(self.toplevel, text='Podaj login: ')
+            user_login_label = ttk.Label(self.toplevel, text='Input login: ')
             user_login_label.place(relx=0.5, rely=0.1, anchor='center')
 
             self.user_login = ttk.Entry(self.toplevel, show='*')
             self.user_login.place(relx=0.5, rely=0.25, anchor='center')
 
-            new_password_label = ttk.Label(self.toplevel, text='Podaj nowe hasło: ')
+            new_password_label = ttk.Label(self.toplevel, text='Input new password: ')
             new_password_label.place(relx=0.5, rely=0.45, anchor='center')
 
             self.new_password = ttk.Entry(self.toplevel, show='*')
             self.new_password.place(relx=0.5, rely=0.60, anchor='center')
 
+            submit_button = ttk.Button(self.toplevel, text='Submit')
+            submit_button.place(relx=0.5, rely=0.85, anchor="center")
+            submit_button.bind('<Button-1>', self.on_submit_button)
 
+    def get_data(self):
+        login_from_user = self.user_login.get()
+        user_password = self.new_password.get()
+        data_item = self.tree.selection()[0]
+        password = api.Api(user_password)
+        valid, errors = main.validate_password(password)
+        if data_item:
+            values = self.tree.item(data_item, 'values')
+            selected_website = values[0]
+
+            session = create_database()
+            credential = session.query(Credential).filter_by(website=selected_website).first()
+            if credential and login_from_user == credential.login:
+                password_obj = credential.passwords
+                if valid:
+                    if password_obj:
+                        password_obj.password = encrypt_password('kacper95', user_password)
+                        session.commit()
+                        messagebox.showinfo('Info', 'Password were updated')
+                else:
+                    error_messages = '\n'.join([f' - {error}\n' for _, error in errors])
+                    messagebox.showerror('Error',
+                                         f'The provided password does not meet the following requirements:\n \n{error_messages}')
+            else:
+                messagebox.showwarning('Warning', 'Incorrect login')
+
+            self.tree.selection_remove(self.item)
+
+    def on_submit_button(self, event):
+        self.get_data()
+        if self.toplevel:
+            self.toplevel.destroy()
