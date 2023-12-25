28948fcff0e7d2191c60c2229a513d622563fe32
Kacper T <k.turek1995@gmail.com>
2023-12-25 23:21:59 +0000

    Updated tree view configuration in the configure_tree method for DeletePassword class in delete_password module

--- delete_password.py
+++ delete_password.py
@@ -55,11 +55,12 @@ class DeletePassword(UpdateTreeview):
         self.configure_tree()
 
     def configure_tree(self):
-        self.tree.column('Choose credential', width=480)
-        self.tree.heading('Choose credential', text='Choose website credential to delete')
+        self.tree['columns'] = ('first',)
+        self.tree.column('first', stretch=tk.YES)
+        self.tree.heading('first', text='Choose website credential to delete')
         self.scrollbar.config(command=self.tree.yview)
-        self.tree.pack()
-        self.scrollbar.place(x=490, rely=0.36, anchor='center', height=99)
+        self.tree.pack(expand=True, fill='both')
+        self.scrollbar.place(x=490, rely=0.32, anchor='center', height=99)
 
         add_tree3_button = ctk.CTkButton(self.tab, text='Submit')
         add_tree3_button.place(relx=0.5, rely=0.9, anchor="center")
