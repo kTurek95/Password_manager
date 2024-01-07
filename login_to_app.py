c0b65329edb95690228b3bca03f6c101a0ceee5c
Kacper <k.turek1995@gmail.com>
2024-01-07 20:10:51 +0000

    Removed 'tab1' and 'func' attributes from the 'LoginToApp' class. Deleted the 'get_value' function and the option to display users in the 'users' tab

--- login_to_app.py
+++ login_to_app.py
@@ -4,10 +4,8 @@ from CTkListbox import *
 from check_password import CheckPassword
 
 class LoginToApp:
-    def __init__(self, tab1, tab2, func, root):
-        self.tab1 = tab1
+    def __init__(self, tab2, root):
         self.tab2 = tab2
-        self.func = func
         self.root = root
         self.username = None
 
@@ -23,17 +21,7 @@ class LoginToApp:
         self.username = ctk.CTkEntry(tab2, width=120)
         self.username.place(relx=0.25, rely=0.45, anchor="center")
 
-        def get_value(selected_option):
-            self.username.delete(0, 'end')
-            self.username.insert(0, selected_option)
-
-        list_box = CTkListbox(tab1, command=get_value)
-        list_box.pack(pady=10)
-        for login in func:
-            list_box.insert(tk.END, '\n'.join(login))
-
         check_password_instance = CheckPassword(self.username, password_from_user, root)
-
 
         add_login_button = ctk.CTkButton(tab2, corner_radius=10, text='Login', width=100)
         add_login_button.place(relx=0.49, rely=0.85, anchor="center")
