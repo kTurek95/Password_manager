18ad3d69a48f13af9d611465f0f7df6bef6b3d82
Kacper <k.turek1995@gmail.com>
2024-01-12 05:15:16 +0000

    Added the 'move_user' function

--- utils.py
+++ utils.py
@@ -14,27 +14,43 @@ def clear_input_fields(*args):
 
 
 def get_users_from_database(username=None):
-        """
-        If a username is provided, 
-        retrieves the user's password. If no username is provided, 
-        retrievesthe user's usernames.
+    """
+    If a username is provided, 
+    retrieves the user's password. If no username is provided, 
+    retrievesthe user's usernames.
 
-        Args:
-            username (str, optional): The username of the user whose password is to be retrieved. 
-                                    If None, all usernames are retrieved. Defaults to None.
+    Parameters:
+        username (str, optional): The username of the user whose password is to be retrieved. 
+                                If None, all usernames are retrieved. Defaults to None.
 
-        Returns:
-            list: A list of usernames if no username is provided. If a username is provided, 
-                returns a list containing the user password.
-        """
-        users = []
-        session = create_database()
-        if not username:
-            all_users = session.query(LoginCredentials).all()
-            for user in all_users:
-                users.append(user.username)
-        else:
-             user = session.query(LoginCredentials).filter_by(username=username).first()
-             users.append(user.password)
+    Returns:
+        list: A list of usernames if no username is provided. If a username is provided, 
+            returns a list containing the user password.
+    """
+    users = []
+    session = create_database()
+    if not username:
+        all_users = session.query(LoginCredentials).all()
+        for user in all_users:
+            users.append(user.username)
+    else:
+        user = session.query(LoginCredentials).filter_by(username=username).first()
+        users.append(user.password)
 
-        return users
+    return users
+
+def move_user(tabview, tab_name):
+    """
+    Switches the user's view to a specified tab in the tabview interface.
+
+    Takes a tabview control (e.g., a ttk.Notebook widget) and a tab name (tab_name),
+    then sets the active tab to the one indicated by tab_name.
+
+    Parameters:
+        tabview: The tab control widget where the active tab is to be changed.
+        tab_name: The name of the tab (string) to switch the view to.
+
+    Returns:
+        None
+    """
+    tabview.set(tab_name)
