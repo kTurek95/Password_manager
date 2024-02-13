4be0c4f41ed61e90dea9603b621bc04921f1fbcd
Kacper <k.turek1995@gmail.com>
2024-02-13 08:00:43 +0000

    Enhancements to the SwitchUser Class
    - Added new fields (user_login, user_password, main_window) to the constructor (__init__) of the SwitchUser class, expanding its configuration capabilities.
    - Initialized the self.tree attribute in the constructor, specifying location, columns, height, enhancing the management of data display.
    - Imported necessary libraries and modules to ensure the proper functioning of the class.
    - Renamed the method from 'new_window' to 'open_new_window' and enhanced its functionality with window centering. Added 'close', 'open_window', 'check_password' functions for improved window management and user authentication.
    - Removed the 'get_users_from_database' method.
    - Modified the 'display_users' function to use the tree (self.tree) initialized in the constructor.

--- switch_user.py
+++ switch_user.py
@@ -1,50 +1,80 @@
 import customtkinter as ctk
-from database import create_database
-from database import LoginCredentials
+from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from utils import get_users_from_database
+from center_window import center_window
+from utils import clear_input_fields
+
 
 class SwitchUser:
-    def __init__(self, tab, root, username):
+    def __init__(self, tab, root, username, user_login, user_password, main_window):
         self.tab = tab
         self.root = root
         self.username = username
+        self.user_login = user_login
+        self.user_password = user_password
+        self.main_window = main_window
+        self.checkboxes = []
+        self.tree = ttk.Treeview(tab,
+                            columns=('Users',),
+                            show='headings',
+                            height=4, )
 
-    def get_users_from_database(self):
-        users = []
-        session = create_database()
-        user_username = session.query(LoginCredentials).filter_by(username=self.username).first()
-        if user_username:
-            all_users = session.query(LoginCredentials).all()
-            for record in all_users:
-                if record.id != user_username.id:
-                    users.append(record.username)
-
-        return users
-
-    def new_window(self):
+    def open_new_window(self):
         new_window = ctk.CTkToplevel(self.root)
         new_window.transient(self.root)
         new_window.grab_set()
-        new_window.geometry('200x100')
+        new_window.geometry('300x200')
+        center_window(new_window)
 
-        password_label=ctk.CTkLabel(new_window, text='Input password:', fg_color=("white", "gray75"), corner_radius=10)
+        password_label=ctk.CTkLabel(new_window, text='Input password:', fg_color=("white", "gray"), corner_radius=10)
         password_label.pack(pady=10)
-        password = ctk.CTkEntry(new_window)
+        password = ctk.CTkEntry(new_window, show='*')
         password.pack()
 
+        username = self.get_selected_item()
+
+        def close():
+            new_window.destroy()
+            self.root.withdraw()
+            self.main_window.withdraw()
+
+        def open_window():
+            from main_window import open_main_window
+            open_main_window(self.main_window, username, self.user_login, self.user_password)
+
+        def check_password():
+            correct_password = password.get()
+            user = get_users_from_database(username=username)
+        
+            if user[0] == correct_password:
+                close()
+                open_window()
+            else:
+                messagebox.showerror('Error', 'Wrong password')
+                clear_input_fields(password)
+
+        submit_button = ctk.CTkButton(new_window, text='ok', command=check_password)
+        submit_button.pack(pady=10)
+
     def display_users(self):
-        label = ctk.CTkLabel(master=self.tab,
-                             text='Choose account do you want to switch:',
-                             corner_radius=10,
-                             fg_color=("white", "gray75"))
-        label.pack(pady=20)
-        users = self.get_users_from_database()
+        self.tree["columns"] = ("1",)
+        self.tree.column("1", stretch=tk.YES)
+
+        self.tree.heading("1", text="Users")
+
+        users = get_users_from_database()
 
         for user in users:
-            my_check = ctk.CTkCheckBox(self.tab, text=user)
-            my_check.pack()
+            if user != self.username:
+                self.tree.insert("", "end", values=(f"{user}",))
 
-        add_new_window_button = ctk.CTkButton(master=self.tab, text='Submit', command=self.new_window)
-        add_new_window_button.pack(pady=20)
+        self.tree.pack(expand=True, fill='both')
+
+        add_new_window_button = ctk.CTkButton(master=self.tab, text='Submit', command= lambda: (self.open_new_window(), self.get_selected_item()))
+        add_new_window_button.place(relx=0.5, rely=0.9, anchor="center")
 
     def get_selected_item(self):
         selected_item = self.tree.selection()
