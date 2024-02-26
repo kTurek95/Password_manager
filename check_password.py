d2b45db2f0edbdc6c3f20adb541bb49d83f572c2
Kacper <k.turek1995@gmail.com>
2024-02-26 11:51:23 +0000

    Implemented key handling from.env file and added password decryption feature to verify the correctness of passowrd in the 'check_password' function

--- check_password.py
+++ check_password.py
@@ -9,6 +9,9 @@ from main_window import open_main_window
 from database import create_database, LoginCredentials
 from utils import clear_input_fields
 from utils import check_if_fields_not_missing
+import os
+from dotenv import load_dotenv
+from cipher_tools import decrypt_password
 
 FAILED_ATTEMPTS = 0
 
@@ -42,15 +45,17 @@ class CheckPassword:
        Returns:
        - bool: True if the user-entered password matches the valid password, False otherwise.
        """
-
+        load_dotenv()
+        key = os.environ.get('KEY')
         session = create_database()
         credentials = session.query(LoginCredentials.username, LoginCredentials.password).all()
-        password_from_user = self.user_password.get()
         username = self.user_login.get()
+        password_from_user = self.user_password.get()
 
 
         for login, password in credentials:
-            if password == password_from_user and login == username:
+            decrypted_password = decrypt_password(key, password)
+            if password_from_user == decrypted_password and login == username:
                 return True
         return False
 
@@ -82,7 +87,7 @@ class CheckPassword:
             open_main_window(self.root, username, self.user_login, self.user_password)
             return True
 
-        if len(user_password) > 1:
+        if len(user_password) > 1 and not self.check_password():
             clear_input_fields(self.user_password, self.user_login)
             FAILED_ATTEMPTS += 1
             messagebox.showwarning('Error', 'Wrong credentials')
